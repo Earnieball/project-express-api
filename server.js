43624da50a1110029865acc1cbba838d07bbd41d
Kim Svensson <110751174+Earnieball@users.noreply.github.com>
2022-11-16 20:35:36 +0100

    just the basic

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import technigoMembers from "./data/technigo-members.json"
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -21,10 +22,26 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  // console.log("req", req);
+  // console.log("res", res);
+  // res.send({responseMessage:"Hello Technigo!"});
+  res.json({responseMessage:"Hello Technigo!"});
+});
+
+app.get("/members", (req, res) => {
+
+  res.status(200).json({technigoMembers: technigoMembers});
+});
+
+app.get("/members/:id", (req, res) => {
+  const singleMember = technigoMembers.find((member) => {
+   return member.id === Number(req.params.id);
+  })
+  res.status(200).json({singleMember: singleMember});
 });
 
 // Start the server
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
