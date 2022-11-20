eedd8baf3fcc42977af8c675b2d0ee2f890d78de
Kim Svensson <110751174+Earnieball@users.noreply.github.com>
2022-11-20 22:56:15 +0100

    final commit

--- server.js
+++ server.js
@@ -1,46 +1,52 @@
 import express from "express";
 import cors from "cors";
-import technigoMembers from "./data/technigo-members.json"
+import booksData from "./data/books.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+
 app.get("/", (req, res) => {
-  // console.log("req", req);
-  // console.log("res", res);
-  // res.send({responseMessage:"Hello Technigo!"});
-  res.json({responseMessage:"Hello Technigo!"});
+
+
+  res.json({responeseMessage: "API about books!"});
 });
 
-app.get("/members", (req, res) => {
-
-  res.status(200).json({technigoMembers: technigoMembers});
+app.get("/books", (req, res) => {
+  res.status(200).json({booksData})
 });
 
-app.get("/members/:id", (req, res) => {
-  const singleMember = technigoMembers.find((member) => {
-   return member.id === Number(req.params.id);
-  })
-  res.status(200).json({singleMember: singleMember});
+app.get("/books/reversed", (req, res) => {
+  const booksDataReversed = booksData.reverse()
+  res.status(200).json({booksDataReversed})
 });
 
-// Start the server
+app.get("/books/:bookID", (req, res) => {
+  const singleBook = booksData.find((book) => {
+    return book.bookID === +req.params.bookID;
+
+  });
+  if(singleBook) {
+    res.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        booksData: singleBook
+      }
+    });
+  } else {
+    res.status(404).json({
+      success: false,
+      message: "Not found",
+      body: {}
+    });
+  }
+
+});
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
